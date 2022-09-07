beeb13f31ba61edd387b1d688d53b373899cc61f
ljk <2052734277@qq.com>
2022-09-07 11:03:19 +0800

    修改登录

--- src/main/java/com/student/Dao/UserDao.java
+++ src/main/java/com/student/Dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.student.Dao;
 
 import com.student.Pojo.User;
+import com.student.utils.JdbcUtils;
 
 import java.sql.*;
 
@@ -10,14 +11,14 @@ public class UserDao {
 
     public User getByName(String userName) {
         User user = new User();
+
+        conn = JdbcUtils.getConnection();
+        String sql = "select * from  user where username = ?";
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cour", "root", "123456");
-            String sql = "select * from  user where username = ?";
             ps = conn.prepareStatement(sql);
-            ps.setString(1,userName);
+            ps.setString(1, userName);
             ResultSet rs = ps.executeQuery();
-            while (rs.next()){
+            while (rs.next()) {
                 user.setId(rs.getInt(1));
                 user.setUserName(rs.getString(2));
                 user.setPassword(rs.getString(3));
@@ -25,24 +26,11 @@ public class UserDao {
 
 
             }
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }finally {
-            try {
-                if(conn!=null){
-                    conn.close();
-                }
-                if(ps!=null){
-                    ps.close();
-                }
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-
+            JdbcUtils.close(ps, conn);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+
 
         return user;
     }
